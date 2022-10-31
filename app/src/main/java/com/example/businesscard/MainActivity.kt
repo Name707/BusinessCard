18551b383a7e0a98489019e6bbe9a2ac1ef416fa
Name707 <707VLAN@gmail.com>
2022-10-31 20:34:06 +0600

    Icons

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.businesscard
 
 
+import android.graphics.Paint
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,18 +17,39 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.PhoneInTalk
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.outlined.Call
+import androidx.compose.material.icons.outlined.Mail
+import androidx.compose.material.icons.outlined.Phone
+import androidx.compose.material.icons.outlined.PhoneInTalk
+import androidx.compose.material.icons.outlined.Send
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material3.Icon
+
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
+import java.time.format.TextStyle
 
 
 class MainActivity : ComponentActivity() {
@@ -39,7 +62,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BusinessCard("123")
+                    BusinessCard()
                 }
             }
         }
@@ -47,9 +70,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BusinessCard(name: String) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+fun BusinessCard() {
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center) {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
             val image = painterResource(R.drawable.android_logo)
             Image(
                 painter = image,
@@ -58,23 +81,69 @@ fun BusinessCard(name: String) {
                     .size(180.dp)
                     .border(BorderStroke(2.dp, Color.Black))
             )
+        }
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            MainText("N707", "Beginner Android Developer ")
 
         }
     }
-
-
-
-
 }
 
 @Composable
-fun BusinessCardText(name: String) {
-Column {
-    Text(
-        text = name,
+fun MainText(myName: String, myProfession: String) {
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = CenterHorizontally) {
+        Text(
+            text = myName,
+            textAlign = TextAlign.Center,
+            color = Color.Magenta,
+            modifier = Modifier
+                .padding(5.dp),
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 30.sp
 
-    )
+        )
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Text(
+                text = myProfession,
+                textAlign = TextAlign.Center,
+                color = Color.Cyan,
+                fontSize = 20.sp,
+
+
+                )
+        }
+        MyContacts("ad", "asd", "asd")
+
+    }
 }
+
+@Composable
+fun MyContacts(number: String, telegram: String, email: String) {
+    Icon(
+        Icons.Outlined.PhoneInTalk,
+        contentDescription = null,
+        tint = Color(red = 233, 150, 122)
+    )
+    Icon(
+        Icons.Default.Send,
+        contentDescription = null,
+        tint = Color(red = 233, 150, 122)
+    )
+    Icon(
+        Icons.Outlined.Mail,
+        contentDescription = null,
+        tint = Color(red = 233, 150, 122)
+    )
+
+}
+
+
+@Composable
+fun BusinessCardText(name: String) {
+    Column {
+
+
+    }
 }
 
 
@@ -82,6 +151,6 @@ Column {
 @Composable
 fun BusinessCardPreview() {
     BusinessCardTheme {
-        BusinessCard("123")
+        BusinessCard()
     }
 }
